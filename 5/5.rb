732aa46398583d35ef26ffdee5b6e31ad590f25c
kyle lau <kylexlau@gmail.com>
2013-02-01 20:09:59 +0800

    add problem 5 ruby solution.

--- 5/5.rb
+++ 5/5.rb
@@ -5,3 +5,25 @@
 
 # What is the smallest positive number that is evenly divisible by all of the 
 # numbers from 1 to 20?
+
+def smallest_multiple(n)
+  m = 1
+  while true
+    n.downto(1) do |i|
+      if (m % i != 0)
+        break
+      end
+      if (i==1)
+        return m
+      end
+    end
+    m += 1
+  end
+end
+
+# Spent about 1 minutes do the computing.
+# There must be a better solution.
+puts smallest_multiple(18)
+
+
+
